6cfcf046d5e765007745d76bf175b2d98a6e10bc
sindhuvahinis <somasundaram.sindhu@gmail.com>
2020-11-21 23:33:05 -0500

    Day 15

---
+++ 65.py
@@ -0,0 +1,71 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+from collections import deque
+
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        queue = deque()
+
+        for n in nestedList:
+            queue.append((n, 1))
+
+        _sum = 0
+
+        while queue:
+            nl, depth = queue.popleft()
+            if nl.isInteger():
+                _sum += nl.getInteger() * depth
+            else:
+                for obj in nl.getList():
+                    queue.append((obj, depth + 1))
+
+        return _sum
+
+
+
+
+
+
+
