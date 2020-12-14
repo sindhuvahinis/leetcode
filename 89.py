55b8c6f6f2a431b8f93cbeafeff9fd0b630ab666
sindhuvahinis <somasundaram.sindhu@gmail.com>
2020-12-14 00:00:50 -0500

    Day 24

---
+++ 89.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maximumAverageSubtree(self, root: TreeNode) -> float:
+        self.res = 0
+
+        def helper(root):
+            if not root:
+                return [0, 0.0]
+
+            count1, sum1 = helper(root.left)
+            count2, sum2 = helper(root.right)
+
+            count = count1 + count2 + 1
+            _sum = sum1 + sum2 + root.val
+            # print(count, _sum)
+
+            self.res = max(self.res, _sum)
+            return [count, _sum]
+
+        helper(root)
+        return self.res
