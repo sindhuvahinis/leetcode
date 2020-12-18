16571b8b211f98a3d704d614128b9d0cc4750d24
sindhuvahinis <somasundaram.sindhu@gmail.com>
2020-12-17 22:57:25 -0500

    Day 26

---
+++ 92.py
@@ -0,0 +1,9 @@
+class Solution:
+    def connectSticks(self, sticks: List[int]) -> int:
+        heapq.heapify(sticks)
+        res = 0
+        while len(sticks) > 1:
+            x, y = heapq.heappop(sticks), heapq.heappop(sticks)
+            res += x + y
+            heapq.heappush(sticks, x + y)
+        return res
